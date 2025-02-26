32192e4415781d2bbf738bee98f5d9f56521771f
Gabin Rolland-Bertrand <94704718+Dantr3b@users.noreply.github.com>
2025-02-26 08:55:46 +0100

    je sais plus ce que j'ai changé

--- V1/js/discover.js
+++ V1/js/discover.js
@@ -1,4 +1,5 @@
 const apiUrl = 'http://localhost:3000/matchs';
+const apiUrl2 = 'http://localhost:3000/cocktail-ingredients-';
 let rejectedMatches = [];
 let currentMatch = null; // Déclaration de currentMatch à l'échelle globale
 
@@ -13,6 +14,24 @@ async function fetchMatchs() {
         }
         currentMatch = await response.json(); // Mettre à jour currentMatch
         displayMatch(currentMatch);
+        fetchInfo();
+    } catch (error) {
+        console.error('Erreur lors de la récupération des matchs:', error);
+    }
+}
+
+async function fetchInfo() {
+    console.log(apiUrl2 + currentMatch.id);
+    try {
+        // Envoyer rejectedMatches comme paramètre de requête
+        const response = await fetch(`${apiUrl2}${currentMatch.id}`);
+        
+        if (!response.ok) {
+            throw new Error('Erreur lors de la récupération des matchs');
+        }
+        currentMatch = await response.json(); // Mettre à jour currentMatch
+        console.log("dka");
+        displayinfo(currentMatch);
     } catch (error) {
         console.error('Erreur lors de la récupération des matchs:', error);
     }
@@ -27,6 +46,24 @@ function displayMatch(match) {
     else{
         matchDiv.querySelector('.ingredients').textContent = "";
     }
+}
+
+function displayinfo(data) {
+    const ingredientsList = document.getElementById('ingredients_list');
+    
+    // Effacer les éléments précédents pour éviter les doublons
+    ingredientsList.innerHTML = '';
+
+    // Ajouter chaque ingrédient en tant qu'élément <li>
+    data.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = `- ${item.ingredientName}`;
+        ingredientsList.appendChild(li);
+    });
+
+    // Ajouter la garniture et les instructions
+    const garnishDiv = document.querySelector('.garniture');
+    garnishDiv.textContent = `Garniture : ${data[0].garnish}`;
 }
 
 function skip() {
@@ -45,5 +82,32 @@ function Make() {
     }
 }
 
+// Fonction pour ouvrir la page d'informations
+function showInfo() {
+    const infoPage = document.getElementById('info-page');
+    if (infoPage.classList.contains('hidden')) {
+      infoPage.classList.remove('hidden');
+      infoPage.classList.add('visible');
+    }else {
+      infoPage.classList.remove('visible');
+      infoPage.classList.add('hidden');
+    }
+  }
+  
+  // Fonction pour fermer la page
+  function closeInfo() {
+    console.log('closeInfo');
+    const infoPage = document.getElementById('info-page');
+    infoPage.classList.remove('visible');
+    infoPage.classList.add('hidden');
+  }
+// Ajout d'un gestionnaire de clic à l'image du cocktail
+document.querySelector('.match').addEventListener('click', showInfo);
+
 // Initialisation au chargement de la page
 window.onload = fetchMatchs;
+
+
+  
+  
+  
